4bfaf22d7c4499ed01e08d00a31eb00b8cb17e73
Guillermo Calvo <guillermo@guillermo.dev>
2023-03-07 11:39:42 +0100

    Add initial project generated with https://launch.micronaut.io/

---
+++ src/main/java/dev/guillermo/TestGhReleaseCommand.java
@@ -0,0 +1,28 @@
+package dev.guillermo;
+
+import io.micronaut.configuration.picocli.PicocliRunner;
+import io.micronaut.context.ApplicationContext;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "test-gh-release", description = "...",
+        mixinStandardHelpOptions = true)
+public class TestGhReleaseCommand implements Runnable {
+
+    @Option(names = {"-v", "--verbose"}, description = "...")
+    boolean verbose;
+
+    public static void main(String[] args) throws Exception {
+        PicocliRunner.run(TestGhReleaseCommand.class, args);
+    }
+
+    public void run() {
+        // business logic here
+        if (verbose) {
+            System.out.println("Hi!");
+        }
+    }
+}
